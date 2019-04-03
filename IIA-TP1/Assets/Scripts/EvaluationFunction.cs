28a478895081641c54e45389343c5fbaf588c56c
Pmsilva1 <pmsilva1@gmail.com>
2019-04-03 17:50:38 +0100

    Added a crap evaluation
    considers the hp and units of both players, more units = bigger impact

--- IIA-TP1/Assets/Scripts/EvaluationFunction.cs
+++ IIA-TP1/Assets/Scripts/EvaluationFunction.cs
@@ -7,12 +7,25 @@ public class EvaluationFunction
     // Do the logic to evaluate the state of the game !
     public float evaluate(State s)
     {
-        float score = 0;
+        float score = 0, hp = 0, units = 0;
 
         for(each unit in s.PlayersUnits)
         {
-            
+            units++;
+            hp += unit.hp;
         }
+
+        score += units*hp;
+        units = 0; hp = 0;
+
+        for(each unit in s.AdversaryUnits)
+        {
+            units++;
+            hp += unit.hp;
+        }
+        
+        score -= units*hp;
+        
         return score;
     }
 }
