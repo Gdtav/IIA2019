c2231f703eaf1b17a9c19575a6f5f6dd03ef287a
Guilherme Tavares <guilhermeditavares@gmail.com>
2019-05-27 23:36:50 +0100

    Add files via upload

---
+++ IIA-TP2/Assets/Scripts/LearningAlgorithms/Tournament.cs
@@ -0,0 +1,25 @@
+﻿using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
+using System;
+
+public class Tournament : SelectionMethod
+{
+    public override List<Individual> selectIndividuals(List<Individual> oldpop, int num, double k)
+    {
+        int i, j;
+        System.Random r = new System.Random();
+        List<Individual> victors = new List<Individual>();
+        while (victors.Count < num)
+        {
+            i = r.Next(oldpop.Count);
+            j = r.Next(oldpop.Count);
+            if (r.NextDouble() > k)
+                victors.Add(oldpop[i].Clone());
+            else
+                victors.Add(oldpop[j].Clone());
+        }
+        return victors;
+    }
+
+}
